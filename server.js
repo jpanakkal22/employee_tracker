d53831ef11cef1fa8dd397d93a875dbb6bd9d6b0
New Dad <josh_panakkal3122@hotmail.com>
2020-08-15 14:56:42 -0400

    Inquirer setup, prompt user to view department, role or employee in database

--- server.js
+++ server.js
@@ -9,13 +9,72 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",  
     // Remember to enter your password!
-    password: "",
+    password: "Contigo%5",
     database: "eTracker_db"
 });
+
+var questions = [
+    {
+        type: "list",
+        message: "Scroll UP or DOWN, press ENTER to select",
+        name: "initial",
+        choices: ["ADD", "VIEW", "UPDATE ROLE"]
+    }
+   
+]
 
 // Connect
 connection.connect(function(err) {
 if (err) throw err;
 console.log("connected as id " + connection.threadId);
-connection.end();
+start();
 });
+
+// Begin Inquirer
+function start(){
+    inquirer.prompt(questions).then((response) => {
+        console.log(response.initial);
+        // Prompt to VIEW
+        if(response.initial === "VIEW"){
+            inquirer.prompt(
+                {
+                    type: "list",
+                    message: "Scroll UP or DOWN, press ENTER to view",
+                    name: "view",
+                    choices: ["Department", "Role", "Employee"]
+                }
+            ).then((response) => {
+                if(response.view === "Department"){
+                    connection.query("SELECT * FROM  department", function (err, res){
+                        if (err) throw err;
+                        
+                        console.table(res);
+                    });
+                }
+                else if(response.view === "Role"){
+                    connection.query("SELECT * FROM  role", function (err, res){
+                        if (err) throw err;
+                        
+                        console.table(res);
+                    });
+                }
+                else {
+                    connection.query("SELECT * FROM  employee", function (err, res){
+                        if (err) throw err;
+                        
+                        console.table(res);
+                    });
+                }  
+            })
+        }
+        // Prompt for ADD
+        else if(response.initial === "ADD"){
+            inquirer.prompt();
+        }
+        // Prompt for UPDATE ROLE
+        else {
+            inquirer.prompt();
+        }            
+    })   
+        
+ }
