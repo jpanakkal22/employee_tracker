e0acf72d82889a4a45c74a08e341137d52a4aab0
New Dad <josh_panakkal3122@hotmail.com>
2020-08-15 13:29:25 -0400

    update comments

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
+// Require dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+// Create mysql connection
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
+    user: "root",  
+    // Remember to enter your password!
     password: "",
     database: "eTracker_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+});
+
+// Connect
+connection.connect(function(err) {
+if (err) throw err;
+console.log("connected as id " + connection.threadId);
+connection.end();
+});
